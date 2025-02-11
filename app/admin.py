63730cc58a8e09c739fb87bedbb71089ab6b5536
Muhammad Qalby <qolbymuhammad86@gmail.com>
2025-02-11 19:38:18 +0800

    Integrating bootstrap template

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from app.models import GeneralInfo
 
 # Register your models here.
+
+@admin.register(GeneralInfo)
+class GeneralInfoAdmin(admin.ModelAdmin):
+  # Untuk menampilkan kolom yang ada di tabel GeneralInfo di dashboard
+  list_display = [
+    'company_name',
+    'location',
+    'email',
+    'phone',
+    'open_hours',
+  ]
+
+  # Untuk menghapus izin menambah data, mengubah data, dan menghapus data
+  # def has_add_permission(self, request: HttpRequest) -> bool:
+  #   # return super().has_add_permission(request)
+  #   return False
+  
+  # def has_change_permission(self, request: HttpRequest, obj=None) -> bool:
+  #   # return super().has_change_permission(request, obj)
+  #   return False
+  
+  # def has_delete_permission(self, request: HttpRequest, obj=None) -> bool:
+  #   # return super().has_delete_permission(request, obj)
+  #   return False
+
+  # Menentukan kolom yang tidak bisa diubah
+  readonly_fields = [
+    'email',
+  ]
--- company/urls.py
+++ company/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import index
+from app.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('app/', index)
+    path('', index, name="home"),
+    path('about/', about, name="about")
 ]
